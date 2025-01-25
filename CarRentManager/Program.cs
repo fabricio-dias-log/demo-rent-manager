dacc9480c06f591454af7e8c6e1c5c5b578d3d74
Fabricio Dias <fabricio270909@gmail.com>
2025-01-24 23:22:30 -0300

    Starting program workflow

--- CarRentManager/Program.cs
+++ CarRentManager/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace CarRentManager;
+﻿using System.Globalization;
+using CarRentManager.Entities;
+
+namespace CarRentManager;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter rental data :");
+        Console.WriteLine("Car model: ");
+        string carModel = Console.ReadLine();
+
+        Console.WriteLine("Pickup (dd/MM/yyyy hh:mm)");
+        DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+        Console.WriteLine("Return (dd/MM/yyyy hh:mm)");
+        DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
+
+        CarRental carRental = new CarRental(start, finish, new Vehicle(carModel));
+
+
+
     }
 }
