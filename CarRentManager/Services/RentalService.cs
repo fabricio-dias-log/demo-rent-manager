5e80cbdc5fa27a679d59803bf75f6c714ef03b8c
Fabricio Dias <fabricio270909@gmail.com>
2025-01-24 23:27:43 -0300

    Structuring service classes

---
+++ CarRentManager/Services/RentalService.cs
@@ -0,0 +1,20 @@
+using CarRentManager.Entities;
+
+namespace CarRentManager.Services;
+
+public class RentalService
+{
+    public double PricePerHour { get; private set; }
+    public double PricePerDay { get; private set; }
+
+    public RentalService(double pricePerHour, double pricePerDay)
+    {
+        PricePerHour = pricePerHour;
+        PricePerDay = pricePerDay;
+    }
+
+    public void ProcessInvoice(CarRental carRental)
+    {
+
+    }
+}
