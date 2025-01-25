32060a6803e81038a29094ce2b180b4272cc39dd
Fabricio Dias <fabricio270909@gmail.com>
2025-01-24 23:08:19 -0300

    Entities is structured

---
+++ CarRentManager/Entities/Vehicle.cs
@@ -0,0 +1,15 @@
+namespace CarRentManager.Entities;
+
+public class Vehicle
+{
+    public string Model { get; set; }
+
+    public Vehicle()
+    {
+    }
+
+    public Vehicle(string model)
+    {
+        Model = model;
+    }
+}
