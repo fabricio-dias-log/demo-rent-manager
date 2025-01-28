4d8e5455220ac8226ef73de4c78fd31fcb68a6e8
Fabricio Dias <fabricio270909@gmail.com>
2025-01-28 17:09:18 -0300

    Fixing object instance and console printing

--- CarRentManager/Program.cs
+++ CarRentManager/Program.cs
@@ -9,23 +9,23 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Enter rental data :");
-        Console.WriteLine("Car model: ");
+        Console.Write("Car model: ");
         string carModel = Console.ReadLine();
 
-        Console.WriteLine("Pickup (dd/MM/yyyy hh:mm)");
+        Console.Write("Pickup (dd/MM/yyyy hh:mm)");
         DateTime start = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
 
-        Console.WriteLine("Return (dd/MM/yyyy hh:mm)");
+        Console.Write("Return (dd/MM/yyyy hh:mm)");
         DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
 
-        Console.WriteLine("Price per hour: ");
+        Console.Write("Price per hour: ");
         double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
-        Console.WriteLine("Price per day: ");
+        Console.Write("Price per day: ");
         double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
         CarRental carRental = new CarRental(start, finish, new Vehicle(carModel));
 
-        RentalService rentalService = new RentalService(hour, day);
+        RentalService rentalService = new RentalService(hour, day, new BrazilTaxService());
 
         rentalService.ProcessInvoice(carRental);
 
