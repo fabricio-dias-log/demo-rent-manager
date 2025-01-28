924769f9215ebe0e248ae54d4fa9b82dfaa1f1ae
Fabricio Dias <fabricio270909@gmail.com>
2025-01-28 16:46:15 -0300

    Program workflow is complete

--- CarRentManager/Program.cs
+++ CarRentManager/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Globalization;
 using CarRentManager.Entities;
+using CarRentManager.Services;
 
 namespace CarRentManager;
 
@@ -17,9 +18,19 @@ class Program
         Console.WriteLine("Return (dd/MM/yyyy hh:mm)");
         DateTime finish = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy HH:mm", CultureInfo.InvariantCulture);
 
+        Console.WriteLine("Price per hour: ");
+        double hour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+        Console.WriteLine("Price per day: ");
+        double day = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
         CarRental carRental = new CarRental(start, finish, new Vehicle(carModel));
 
+        RentalService rentalService = new RentalService(hour, day);
 
+        rentalService.ProcessInvoice(carRental);
 
+        Console.WriteLine();
+        Console.WriteLine("INVOICE:");
+        Console.WriteLine(carRental.Invoice);
     }
 }
